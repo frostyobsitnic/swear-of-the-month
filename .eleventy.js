0a8528d216ce894261c1c7eedcf20524334515d2
frostyobsitnic <lordofthescript@gmail.com>
2021-02-05 22:44:02 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,35 +10,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  
-function extractExcerpt(article) {
-  if (!article.hasOwnProperty('templateContent')) {
-    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-    return null;
-  }
- 
-  let excerpt = null;
-  const content = article.templateContent;
- 
-  // The start and end separators to try and match to extract the excerpt
-  const separatorsList = [
-    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
-    { start: '<p>', end: '</p>' }
-  ];
- 
-  separatorsList.some(separators => {
-    const startPosition = content.indexOf(separators.start);
-    const endPosition = content.indexOf(separators.end);
- 
-    if (startPosition !== -1 && endPosition !== -1) {
-      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-      return true; // Exit out of array loop on first match
-    }
-  });
- 
-  return excerpt;
-}
 
+  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+  
   eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
@@ -156,4 +130,32 @@ function extractExcerpt(article) {
       output: "_site"
     }
   };
+};
+
+function extractExcerpt(article) {
+  if (!article.hasOwnProperty('templateContent')) {
+    console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+    return null;
+  }
+ 
+  let excerpt = null;
+  const content = article.templateContent;
+ 
+  // The start and end separators to try and match to extract the excerpt
+  const separatorsList = [
+    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+    { start: '<p>', end: '</p>' }
+  ];
+ 
+  separatorsList.some(separators => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
+ 
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+      return true; // Exit out of array loop on first match
+    }
+  });
+ 
+  return excerpt;
 };
